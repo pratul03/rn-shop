8381de67967a36ac860700e83da23026c0975ad4
Pratul Makar <pratulmakar02@gmail.com>
2024-10-28 20:33:40 +0530

    some static changes

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,20 +1,58 @@
 import { Tabs } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof Feather>["name"];
+  color: string;
+}) {
+  return <Feather size={24} {...props} style={{ color: "#1BC464" }} />;
+}
 
 const TabsLayout = () => {
   return (
-    <SafeAreaView>
-      <Tabs>
+    <SafeAreaView edges={["top"]} style={styles.safeArea}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#1BC646",
+          tabBarInactiveTintColor: "gray",
+          tabBarLabelStyle: { fontSize: 16 },
+          tabBarStyle: {
+            padding: 2,
+            borderTopLeftRadius: 5,
+            borderTopRightRadius: 5,
+          },
+          headerShown: false,
+        }}
+      >
         <Tabs.Screen
           name="index"
           options={{
-            headerShown: false,
+            title: "shop",
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name="shopping-cart" />;
+            },
           }}
         />
-        <Tabs.Screen name="orders" options={{}} />
+        <Tabs.Screen
+          name="orders"
+          options={{
+            title: "orders",
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name="shopping-bag" />;
+            },
+          }}
+        />
       </Tabs>
     </SafeAreaView>
   );
 };
 
 export default TabsLayout;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
