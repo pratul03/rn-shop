b08565ff12bcd0eb469e985ddd622d9688eb0636
Pratul Makar <pratulmakar02@gmail.com>
2024-10-28 18:37:59 +0530

    learning REACT_NATIVE

---
+++ src/app/(shop)/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
