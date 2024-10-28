30abbab5ebdd0d2ee7febedfb063969522a4daea
Pratul Makar <pratulmakar02@gmail.com>
2024-10-28 19:49:26 +0530

    new pages

---
+++ src/app/product/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Product = () => {
+  return (
+    <View>
+      <Text>Product</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Product;
