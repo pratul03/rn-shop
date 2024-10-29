c9aed55b06b78d4582c2fb642f6e6d1b46e184bf
Pratul Makar <pratulmakar02@gmail.com>
2024-10-29 22:08:39 +0530

    working on cart page

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,9 +1,201 @@
-import { Text, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useCartStore } from "../store/cart-store";
+import { StatusBar } from "expo-status-bar";
+import Feather from "@expo/vector-icons/Feather";
+
+type CartItemType = {
+  id: number;
+  title: string;
+  image: any;
+  price: number;
+  quantity: number;
+};
+
+type CartItemProps = {
+  item: CartItemType;
+  onRemove: (id: number) => void;
+  onIncrement: (id: number) => void;
+  onDecrement: (id: number) => void;
+};
+
+const CartItem = ({
+  item,
+  onDecrement,
+  onIncrement,
+  onRemove,
+}: CartItemProps) => {
+  return (
+    <View style={styles.cartItem}>
+      <Image source={item.image} style={styles.itemImage} />
+      <View style={styles.itemDetails}>
+        <Text style={styles.itemTitle}>{item.title}</Text>
+        <Text style={styles.itemPrice}>₹{item.price.toFixed(2)}</Text>
+        <View style={styles.quantityContainer}>
+          <TouchableOpacity
+            onPress={() => onDecrement(item.id)}
+            style={styles.quantityButton}
+          >
+            <Text style={styles.quantityButtonText}>
+              <Feather name="minus" size={18} color="black" />
+            </Text>
+          </TouchableOpacity>
+          <Text style={styles.itemQuantity}>{item.quantity}</Text>
+          <TouchableOpacity
+            onPress={() => onIncrement(item.id)}
+            style={styles.quantityButton}
+          >
+            <Text style={styles.quantityButtonText}>
+              <Feather name="plus" size={18} color="black" />
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity
+        onPress={() => onRemove(item.id)}
+        style={styles.removeButton}
+      >
+        <Text style={styles.removeButtonText}>Remove</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default function Cart() {
+  const { items, removeItem, incrementItem, decrementItem, getTotalPrice } =
+    useCartStore();
+
+  const handleCheckOut = () => {
+    Alert.alert("Proceeding to checkout", `Total amount: ₹ ${getTotalPrice()}`);
+  };
   return (
-    <View>
-      <Text>Cart</Text>
+    <View style={styles.container}>
+      <StatusBar style={Platform.OS === "android" ? "dark" : "auto"} />
+
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <CartItem
+            item={item}
+            onRemove={removeItem}
+            onIncrement={incrementItem}
+            onDecrement={decrementItem}
+          />
+        )}
+        contentContainerStyle={styles.cartList}
+      />
+
+      <View style={styles.footer}>
+        <Text style={styles.totalText}>Total: ₹{getTotalPrice()}</Text>
+        <TouchableOpacity
+          style={styles.checkoutButton}
+          onPress={handleCheckOut}
+        >
+          <Text style={styles.checkoutButtonText}>Checkout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
+  },
+  cartList: {
+    paddingVertical: 16,
+  },
+  cartItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 16,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: "#f9f9f9",
+  },
+  itemImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 8,
+  },
+  itemDetails: {
+    flex: 1,
+    marginLeft: 16,
+  },
+  itemTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+  itemPrice: {
+    fontSize: 16,
+    color: "#888",
+    marginBottom: 4,
+  },
+  itemQuantity: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#666",
+  },
+  removeButton: {
+    padding: 8,
+    backgroundColor: "#ff5252",
+    borderRadius: 4,
+  },
+  removeButtonText: {
+    fontWeight: "500",
+    color: "#fff",
+    fontSize: 14,
+  },
+  footer: {
+    borderTopWidth: 1,
+    borderColor: "#ddd",
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    alignItems: "center",
+  },
+  totalText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  checkoutButton: {
+    backgroundColor: "#28a745",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+  },
+  checkoutButtonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  quantityContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  quantityButton: {
+    width: 30,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    backgroundColor: "#ddd",
+    marginHorizontal: 5,
+  },
+  quantityButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
