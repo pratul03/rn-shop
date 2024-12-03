2dcc015b8afcd4096fe13e9d5592a172c222465e
pratul03 <kk953504@gmail.com>
2024-12-03 20:16:22 +0530

    nd

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -17,9 +17,10 @@ import Feather from "@expo/vector-icons/Feather";
 type CartItemType = {
   id: number;
   title: string;
-  image: any;
+  heroImage: string;
   price: number;
   quantity: number;
+  maxQuantity: number;
 };
 
 type CartItemProps = {
@@ -37,7 +38,7 @@ const CartItem = ({
 }: CartItemProps) => {
   return (
     <View style={styles.cartItem}>
-      <Image source={item.image} style={styles.itemImage} />
+      <Image source={{ uri: item.heroImage }} style={styles.itemImage} />
       <View style={styles.itemDetails}>
         <Text style={styles.itemTitle}>{item.title}</Text>
         <Text style={styles.itemPrice}>₹{item.price.toFixed(2)}</Text>
--- src/app/product/[slug].tsx
+++ src/app/product/[slug].tsx
@@ -65,9 +65,10 @@ const ProductDetails = () => {
     addItem({
       id: product.id,
       title: product.title,
-      image: product.heroImage,
+      heroImage: product.heroImage,
       price: product.price,
       quantity,
+      maxQuantity: product.maxQuantity,
     });
     toast.show("Added to cart", {
       type: "success",
