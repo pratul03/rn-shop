dbdee426883383d4de76784fd8c1726f07133a7d
pratul03 <kk953504@gmail.com>
2024-12-07 19:29:53 +0530

    nd

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "../lib/supabase";
+import { useAuth } from "../providers/auth-provider";
 
 export const getProductsAndCategories = () => {
   return useQuery({
@@ -60,6 +61,30 @@ export const getCategoryAndProducts = (categorySlug: string) => {
       }
 
       return { category, products };
+    },
+  });
+};
+
+export const getMyOrders = () => {
+  const {
+    user: { id },
+  } = useAuth();
+
+  return useQuery({
+    queryKey: ["orders", id],
+    queryFn: async () => {
+      const { data, error } = await supabase
+        .from("order")
+        .select("*")
+        .order("created_at", { ascending: false })
+        .eq("user", id);
+
+      if (error)
+        throw new Error(
+          `An error occurred while fetching orders : ${error.message}`
+        );
+
+      return data;
     },
   });
 };
