777fcd8feeecdffc4ea8e56e446cbdbc130b3b17
pratul03 <kk953504@gmail.com>
2024-12-11 00:32:52 +0530

    fixed order page

--- src/app/(shop)/orders/_layout.tsx
+++ src/app/(shop)/orders/_layout.tsx
@@ -1,6 +1,8 @@
 import { Stack } from "expo-router";
+import { useOrderUpdateSubscription } from "../../../api/subscription";
 
 export default function OrdersLayout() {
+  useOrderUpdateSubscription();
   return (
     <Stack>
       <Stack.Screen
